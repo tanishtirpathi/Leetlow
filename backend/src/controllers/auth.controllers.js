952ee37b38da311630f35830af81a0f9b6c06b0d
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-11-07 00:03:51 +0530

    fixed small bug: error in generateing prisma files

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,19 @@
 import brcypt from "bcryptjs";
-
-const regester = async (req, res) => {};
+import {ApiError} from "../libs/apierror.js";
+import {db} from "../libs/db.js";
+const regester = async (req, res) => {
+  const { email, name, password } = req.body;
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      throw new DApiError(401, "user already exist ");
+    }
+  } catch (error) {}
+};
 const login = async (req, res) => {};
 const logout = async (req, res) => {};
 const checkUser = async (req, res) => {};
